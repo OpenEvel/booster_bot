15e2f029e4683127b3701fd2635aa6e26472f0b7
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-11-26 14:25:44 +0300

    Первая проба добавить aiogram

--- bot_tlg/logic/__init__.py
+++ bot_tlg/logic/__init__.py
@@ -1,27 +1,54 @@
 from django.conf import settings
 from ..models import Profile
 from ..models import Message
+from aiogram import Dispatcher, Bot, types
 import telebot
 
-bot = telebot.TeleBot(settings.TOKEN)
+# bot = telebot.TeleBot(settings.TOKEN)
 
-@bot.message_handler()
-def cmd(message):
+class BotsDispathcer(Bot):
+    all = []
+
+    def __init__(self, *args, **kargs):
+        # Добавляем этот диспетчер в список всех диспетчеров
+        BotsDispathcer.all.append(self)
+        super().__init__(self, *args, **kargs)
+    
+    @classmethod
+    def message_handler_lol(cls, *custom_filters, commands=None, regexp=None, content_types=None, state=None,
+                        run_task=None, **kwargs):
+        def decorator(callback):
+            for dp in BotsDispathcer.all:
+                dp.register_message_handler(callback, *custom_filters,
+                                          commands=commands, regexp=regexp, content_types=content_types,
+                                          state=state, run_task=run_task, **kwargs)
+            return callback
+
+        return decorator
+
+
+@BotsDispathcer.message_handler_lol()
+async def cmd(message: types.Message):
     chat_id = message.chat.id
     text = message.text
+    print(text)
 
-    p, _ = Profile.objects.get_or_create(
-        external_id=chat_id,
-        defaults={
-            'name': message.from_user.username
-        }
-    )
-    # Сохраняем сообщение в базу
-    Message(profile=p, text=text).save()
+# def cmd(message):
+#     chat_id = message.chat.id
+#     text = message.text
 
-    messages =  Message.objects.filter(profile=p)
-    count = messages.count()
+#     p, _ = Profile.objects.get_or_create(
+#         external_id=chat_id,
+#         defaults={
+#             'name': message.from_user.username
+#         }
+#     )
+#     # Сохраняем сообщение в базу
+#     Message(profile=p, text=text).save()
 
-    all_mess = '\n'.join([m.text for m in messages])
+#     messages =  Message.objects.filter(profile=p)
+#     count = messages.count()
 
-    bot.send_message(chat_id, f'У вас {count} сообщений в базе\n{all_mess}')
+#     all_mess = '\n'.join([m.text for m in messages])
+
+#     bot.send_message(chat_id, f'У вас {count} сообщений в базе\n{all_mess}')
--- bot_tlg/management/commands/runbot.py
+++ bot_tlg/management/commands/runbot.py
@@ -1,7 +1,28 @@
 from django.core.management.base import BaseCommand
-from bot_tlg.logic import bot
+from bot_tlg.logic import BotsDispathcer
+from bot_tlg.models import TlgBot
+from aiogram import Bot, executor, Dispatcher
+import asyncio
+
+def main():
+    all_bots = TlgBot.objects.all()
+    for bot in all_bots:
+        b = Bot(bot.token)
+        # dp = BotsDispathcer(b)
+        # executor.start_polling(dp, skip_updates=True)
+
+async def lol(TOKEN):
+    bot = Bot(TOKEN)
+    try:
+        me = await bot.get_me()
+        print(f'{me.username} запущен')
+    finally:
+        await bot.close()
+
+
 class Command(BaseCommand):
     help = 'Запустить телеграм-бот'
 
     def handle(self, *args, **options):
-        bot.infinity_polling()
+        TOKEN = TlgBot.objects.all()[0].token
+        asyncio.run(lol(TOKEN))
